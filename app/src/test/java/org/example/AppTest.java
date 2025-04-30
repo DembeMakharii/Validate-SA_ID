024678f328a736feb9c2f53bf9a32a41b1299fb8
Dembe Makhari <dembemakhari67@gmail.com>
2025-04-30 13:14:27 +0200

    adding a test method

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -3,12 +3,13 @@
  */
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @Test
+    void testValidIdNumbers() {
+        assertTrue(ValidateSAid.isValid("2001014800086"));
+        assertTrue(ValidateSAid.isValid("2909035800085"));
     }
 }
